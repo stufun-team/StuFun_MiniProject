963f5dbe98ed8c475a6045e34e361050642cc0c9
tushar-manish <tushargupta9412@gmail.com>
2020-10-18 20:20:09 +0530

    Login/Register Activity

--- app/src/main/java/com/example/stufun/UserSelection.java
+++ app/src/main/java/com/example/stufun/UserSelection.java
@@ -4,13 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.stufun.R;
+
+public class UserSelection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_user_selection);
     }
 }
